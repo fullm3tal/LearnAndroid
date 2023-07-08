8495c95118424de11322799e937b7182328f8d7d
Bheem Negi <bheem.negi3@gmail.com>
2023-07-08 20:28:47 +0530

    feat: add picker library

--- app/src/main/java/com/leaphorizon/learnandroid/MainActivity.kt
+++ app/src/main/java/com/leaphorizon/learnandroid/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.leaphorizon.learnandroid
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.atwa.filepicker.core.FilePicker
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private val filePicker = FilePicker.getInstance(this)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tvClickNotification).setOnClickListener {
+            pickImage()
+        }
     }
+
+    fun pickImage() {
+        filePicker.pickImage() { meta ->
+        }
+    }
+
 }
